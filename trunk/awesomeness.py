8af7e5fd33b91c68e937ee9a86f175b498d4a24f
stu940652 <stu940652@0a505772-8c2b-487d-83c2-34df072d0b86>
2015-09-22 01:31:57 +0000

    Can load and save presets. Error checking is fairly limited

--- trunk/awesomeness.py
+++ trunk/awesomeness.py
@@ -1,4 +1,5 @@
 import wx
+import configparser
 from AJArest import kumo
 
 class kumoManager (kumo.Client):
@@ -31,6 +32,88 @@ class kumoManager (kumo.Client):
         # print ("setChannel", destination, source)
         # print c.setParameter('eParamID_XPT_Destination3_Status','9')
 
+class SaveSettings (wx.Frame):
+    def __init__ (self, parent, settings, namesDst, namesSrc):
+        wx.Frame.__init__(self, parent, -1, "Save Preset")
+        panel = wx.Panel(self)
+        self.settings = settings
+        self.parent = parent
+        print (settings)
+        
+        mainSizer = wx.BoxSizer(wx.VERTICAL)
+        mainSizer.Add(wx.StaticText(panel, -1, "Save the following settings to a Preset"), border = 5, flag = wx.ALL)
+        sizer = wx.FlexGridSizer(2, 5, 15)
+        sizer.Add(wx.StaticText(panel, -1, "Destination"))
+        sizer.Add(wx.StaticText(panel, -1, "Source"))
+    
+        for s in settings.split(','):
+            destination, source = s.split('=')
+            destination = int(destination)
+            source = int(source)
+            sizer.Add (wx.StaticText(panel, -1, namesDst[destination] + " ="))
+            sizer.Add (wx.StaticText(panel, -1, namesSrc[source]))
+
+        mainSizer.Add(sizer, border = 5, flag = wx.ALL)
+        
+        mainSizer.Add(wx.StaticText(panel, -1, "Preset Name"), border = 5, flag = wx.TOP)
+        self.PresetName = wx.TextCtrl(panel)
+        mainSizer.Add(self.PresetName, flag = wx.EXPAND)
+        
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.btnOk = wx.Button (panel, -1, "Save")
+        self.Bind(wx.EVT_BUTTON, self.OnSave, self.btnOk)
+        sizer.Add(self.btnOk)
+        btnCancel = wx.Button (panel, -1, "Cancel")
+        self.Bind(wx.EVT_BUTTON, self.OnClose, btnCancel)
+        sizer.Add(btnCancel)
+        
+        mainSizer.Add(sizer, flag = wx.ALIGN_RIGHT)
+        panel.SetSizer(mainSizer)
+        panel.Layout()
+        # And also use a sizer to manage the size of the panel such
+        # that it fills the frame
+        sizer = wx.BoxSizer()
+        sizer.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+        self.Fit()
+        
+    def OnClose (self, evt):
+        self.Close()
+        
+    def OnSave (self, evt):
+        # Get preset settings if they exist
+        presets=configparser.SafeConfigParser()
+        presets.optionxform = lambda option: option
+        # And read from the ini file
+        try:
+            presets.read(presetFileName)
+        except:
+            pass
+            
+        presetName = self.PresetName.GetValue().strip().replace(":","")
+
+        if presetName in presets["KumoPresets"]:
+            pass
+            # option to cancel if it exists
+            d = wx.MessageDialog(self, 'Preset "' + presetName + '" already exists. \nDo you want to overwrite?',
+                                style = wx.OK | wx.CANCEL)
+            
+            if d.ShowModal() != wx.ID_OK:
+                return
+                
+        # Add this preset
+        presets["KumoPresets"][presetName] = self.settings
+        
+        # And save
+        with open(presetFileName, 'w') as configfile:
+            presets.write(configfile)
+        
+        # Update parents list of presets
+        self.parent.UpdatePresetList()
+        
+        # Done
+        self.Close()
+
 class MyFrame(wx.Frame):
     """
     This is MyFrame.  It just shows a few controls on a wxPanel,
@@ -59,7 +142,6 @@ class MyFrame(wx.Frame):
 
         self.CreateStatusBar()
         
-
         # Now create the Panel to put the other controls on.
         panel = wx.Panel(self)
         
@@ -68,7 +150,23 @@ class MyFrame(wx.Frame):
         self.kumo.getNames()
         self.kumo.getSettings()
         
+        panelSizer = wx.BoxSizer(wx.VERTICAL)
+        self.PresetSelection = wx.ComboBox(panel)
+        panelSizer.Add(self.PresetSelection, flag=wx.EXPAND)
+        self.UpdatePresetList()
+        
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        presetLoad = wx.Button(panel, -1, "Load Preset")
+        self.Bind(wx.EVT_BUTTON, self.OnLoad, presetLoad)
+        sizer.Add(presetLoad)
+        presetSave = wx.Button(panel, -1, "Save Preset")
+        self.Bind(wx.EVT_BUTTON, self.OnSave, presetSave)
+        sizer.Add(presetSave)
+        panelSizer.Add(sizer)
+        panelSizer.AddSpacer(10)
+        
         sizer = wx.FlexGridSizer(3, 5, 15)
+        
         sizer.Add(wx.StaticText(panel, -1, "Destination"))
         sizer.Add(wx.StaticText(panel, -1, "Next Source"))
         self.globalEnableSource = wx.CheckBox(panel, -1, label = "Current Source")
@@ -102,7 +200,8 @@ class MyFrame(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnUpdate, updateButton)
         sizer.Add(updateButton)
         
-        panel.SetSizer(sizer)
+        panelSizer.Add(sizer, flag = wx.EXPAND)
+        panel.SetSizer(panelSizer)
         panel.Layout()
 
         # And also use a sizer to manage the size of the panel such
@@ -111,29 +210,73 @@ class MyFrame(wx.Frame):
         sizer.Add(panel, 1, wx.EXPAND)
         self.SetSizer(sizer)
         self.Fit()
-        self.CenterOnScreen(wx.BOTH)
         
         # Start a timer to get latest setting from Kumo
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
         self.timer.Start(2e+3) # 2 second interval
+        
+    def UpdatePresetList (self):
+        # Get preset settings if they exist
+        self.Presets=configparser.SafeConfigParser()
+        self.Presets.optionxform = lambda option: option
+        # And read from the ini file
+        presetNames=[]
+        try:
+            self.Presets.read(presetFileName)
+            presetNames = self.Presets.options("KumoPresets")
+        except:
+            pass
+            
+        self.PresetSelection.Set(presetNames)
     
     def UpdateCurrent (self):
         self.kumo.getSettings()
         for dest in self.destControls:
             self.destControls[dest]["enableSource"].SetLabel(self.kumo.namesSrc[self.kumo.destSet[dest]])
+
+    def OnSave (self, evt):
+        settings = ""
+        for dest in self.destControls:
+            if self.destControls[dest]["enableSource"].IsChecked():
+                settings += "%i=%i," % (dest, self.destControls[dest]["nextSource"].GetSelection()+1)
+        settings = settings.strip(',')
         
+        m = SaveSettings(self, settings, self.kumo.namesDst, self.kumo.namesSrc)
+        m.Show(True)
+    
+    def OnLoad (self, evt):
+        settings = self.Presets["KumoPresets"][self.PresetSelection.GetValue()]
+        for s in settings.split(','):
+            destination, source = s.split('=')
+            dest = int(destination)
+            source = int(source)
+            self.destControls[dest]["nextSource"].SetSelection(source)
+            if self.destControls[dest]["nextSource"].GetValue() == self.destControls[dest]["enableSource"].GetLabel():
+                self.destControls[dest]["enableSource"].SetValue(False)
+            else:
+                self.destControls[dest]["enableSource"].SetValue(True)
+
     def OnApply (self, evt):   
         for dest in self.destControls:
             if self.destControls[dest]["enableSource"].IsChecked():
                 self.kumo.setChannel(dest, self.destControls[dest]["nextSource"].GetSelection()+1)
                 
+        # Clear check boxes
+        for dest in self.destControls:
+            self.destControls[dest]["enableSource"].SetValue(self.globalEnableSource.GetValue())
+
+        # Update        
         self.UpdateCurrent()
         
     def OnUpdate (self, evt):
         self.UpdateCurrent()
         for dest in self.destControls:
             self.destControls[dest]["nextSource"].SetSelection(self.kumo.destSet[dest]-1)
+
+        # Clear check boxes
+        for dest in self.destControls:
+            self.destControls[dest]["enableSource"].SetValue(self.globalEnableSource.GetValue())
             
     def OnTimer (self, evt):
         self.UpdateCurrent()
@@ -141,7 +284,10 @@ class MyFrame(wx.Frame):
     def OnSelectSource (self, evt):
         for dest in self.destControls:
             if self.destControls[dest]["nextSource"] == evt.GetEventObject():
-                self.destControls[dest]["enableSource"].SetValue(True)
+                if self.destControls[dest]["nextSource"].GetValue() == self.destControls[dest]["enableSource"].GetLabel():
+                    self.destControls[dest]["enableSource"].SetValue(False)
+                else:
+                    self.destControls[dest]["enableSource"].SetValue(True)
         
     def OnGlobalCheck (self, evt):            
         for dest in self.destControls:
