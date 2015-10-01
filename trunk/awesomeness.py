7cd82e510b4a628f1991cec445ccc2f9582ace77
stu940652 <stu940652@0a505772-8c2b-487d-83c2-34df072d0b86>
2015-10-01 02:38:51 +0000

    Rough in for addition of KiPro panel.

--- trunk/awesomeness.py
+++ trunk/awesomeness.py
@@ -1,9 +1,54 @@
 import wx
 from frame_kumo import FrameKumo
 
+class MainFrame (wx.Frame):
+    """
+    This is MyFrame.  It just shows a few controls on a wxPanel,
+    and has a simple menu.
+    """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, -1, title)
+
+        # Create the menubar
+        menuBar = wx.MenuBar()
+
+        # and a menu 
+        menu = wx.Menu()
+
+        # add an item to the menu, using \tKeyName automatically
+        # creates an accelerator, the third param is some help text
+        # that will show up in the statusbar
+        menu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Exit this simple sample")
+
+        # bind the menu event to an event handler
+        #self.Bind(wx.EVT_MENU, self.OnTimeToClose, id=wx.ID_EXIT)
+
+        # and put the menu on the menubar
+        menuBar.Append(menu, "&File")
+        self.SetMenuBar(menuBar)
+        
+        #splitter = wx.SplitterWindow(self, style=wx.SP_BORDER)
+        #panelKumo = FrameKumo(splitter)
+        #blank = wx.Panel(splitter)
+        #splitter.SplitVertically(panelKumo, blank, 200)
+
+        panelKumo = FrameKumo(self)
+        blank = wx.Panel(self)
+        
+        # And also use a sizer to manage the size of the panel such
+        # that it fills the frame
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        #sizer.Add(splitter, 1, wx.EXPAND)
+        sizer.Add(panelKumo, 1, wx.EXPAND)
+        sizer.Add(blank, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+        self.Fit()
+        
+
+
 class MyApp(wx.App):
     def OnInit(self):
-        frame = FrameKumo(None, "Awesomeness")
+        frame = MainFrame(None, "Awesomeness")
         self.SetTopWindow(frame)
         frame.Show(True)
         return True
