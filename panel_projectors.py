3bfb14d1cd4f5e9eaa7627f8dab78056d81bbee2
Andy Martin <25186508+STU940652@users.noreply.github.com>
2015-11-02 18:48:17 +0000

    Did some stuff.  Untested.

--- panel_projectors.py
+++ panel_projectors.py
@@ -13,90 +13,53 @@ class PanelProjector (wx.lib.scrolledpanel.ScrolledPanel):
 
         self.infoBar = wx.InfoBar(self)
         
+        panelSizer = wx.BoxSizer(wx.VERTICAL)
+        sizer = wx.wx.FlexGridSizer(cols = 3)
+        
         # Init the connection
         for i in range(5):
             host = Settings.Config.get("projector","ip%i" % i, None)
-            port = 4352
-            password = 'panasonic' # default password
+            get_password = lambda: 'panasonic' # default password
+            
             if host == None:
                 break
             try:
-                sock = socket()
-                sock.connect((host, port))
-                f = sock.makefile()
-
-                get_password = lambda: password
-                proj = Projector(f)
+                proj = Projector(host)
                 rv = proj.authenticate(get_password)
                 if rv is False:
                     print('Incorrect password.', file=sys.stderr)
-                projectors.append(proj)
+                    
             except:
                 traceback.print_exc()
-
-        panelSizer = wx.BoxSizer(wx.VERTICAL)
-        
-        for thisProjector in projectors:
-            pass
-        
-        
-        
                 
-        # Program Output
-        self.AUX_radio = wx.RadioBox(self,label = "AUX: Auxilliary", choices = ["1","2","3","4","5","FMEM1","FMEM2","PGM"])
-        self.Bind(wx.EVT_RADIOBOX, self.OnChangeOutput, self.AUX_radio)
-        panelSizer.Add(self.AUX_radio, border = 5, flag=wx.EXPAND|wx.ALL)       
-        
-        self.PGM_radio = wx.RadioBox(self,label = "PGM: Program", choices = ["1","2","3","4","5","FMEM1","FMEM2"])
-        self.Bind(wx.EVT_RADIOBOX, self.OnChangeOutput, self.PGM_radio)
-        panelSizer.Add(self.PGM_radio, border = 5, flag=wx.EXPAND|wx.ALL)
+            # Built the UI
+            name = Settings.Config.get("projector", "name%i" % i, str(i))
+            sizer.Add(wx.StaticText(self, -1, name))
+            
+            onoffText = wx.StaticText(self, -1, "Offline")
+            sizer.Add(onoffText)
+            
+            shutterCheck = wx.CheckBox(self, -1, label = "Shuttered")
+            self.Bind(wx.EVT_CHECKBOX, self.OnShutter, shutterCheck)
+            sizer.Add(shutterCheck)
+                
+            self.projectors.append( (proj, onoffText, shutterCheck) )
 
-        self.PVW_radio = wx.RadioBox(self,label = "PVW: Preview", choices = ["1","2","3","4","5","FMEM1","FMEM2"])
-        self.Bind(wx.EVT_RADIOBOX, self.OnChangeOutput, self.PVW_radio)
-        panelSizer.Add(self.PVW_radio, border = 5, flag=wx.EXPAND|wx.ALL)
-
         
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-        sizer.AddStretchSpacer(1)
-        self.cutButton = wx.Button (self, -1, "Cut")
-        self.Bind(wx.EVT_BUTTON, self.OnCut, self.cutButton)
-        sizer.Add(self.cutButton)
-        sizer.AddStretchSpacer(1)
-        self.fadeButton = wx.Button (self, -1, "Auto Fade")
-        self.Bind(wx.EVT_BUTTON, self.OnFade, self.fadeButton)
-        sizer.Add(self.fadeButton)
-        sizer.AddStretchSpacer(1)
-        self.ftbButton = wx.Button (self, -1, "Fade to Black")
-        self.Bind(wx.EVT_BUTTON, self.OnFTB, self.ftbButton)
-        sizer.Add(self.ftbButton)
-        sizer.AddStretchSpacer(1)
         panelSizer.Add(sizer, border = 5, flag=wx.EXPAND)
         
         panelSizer.AddStretchSpacer()
         panelSizer.Add(self.infoBar, flag = wx.EXPAND)
         self.SetSizer(panelSizer)
         self.Layout()
-        #wx.lib.scrolledpanel.ScrolledPanel.SetupScrolling(self)
         
         # Start a timer to get latest setting from HS50
         self.timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
         self.timer.Start(0.2e+3) # 0.2 second interval
 
-    def OnChangeOutput (self, evt):
-        if evt.GetEventObject() == self.AUX_radio:
-            bus = b"12"
-        elif evt.GetEventObject() == self.PGM_radio:
-            bus = b"02"
-        elif evt.GetEventObject() == self.PVW_radio:
-            bus = b"03"
-        else:
-            return
-                    
-        c = STX + b"SBUS:" + bus + b":" + self.inputList[evt.GetEventObject().GetSelection()] + ETX
-        if self.socket:
-            self.socket.sendall(c)
-        #print (c)
+    def OnShutter (self, evt):
+        pass
         
     def OnTimer (self, evt):
         pass
