194e945f89a71b3abc7bbb081503aa4d62bf099e
stu940652 <stu940652@0a505772-8c2b-487d-83c2-34df072d0b86>
2016-11-14 03:02:51 +0000

    Scrubber seems to be working for some reason.

--- trunk/panel_kipro.py
+++ trunk/panel_kipro.py
@@ -705,7 +705,12 @@ class PanelKipro (wx.lib.scrolledpanel.ScrolledPanel):
             slideFrame = timecode_to_frames(self.currentClipInfo["attributes"]["Starting TC"], fps) + \
                         slideRatio * int(self.currentClipInfo["framecount"])
             # Some debug info
-            print (fps, slideRatio, slideFrame, timecode_to_frames(self.currentClipInfo["attributes"]["Starting TC"], fps), int(self.currentClipInfo["framecount"], frames_to_timecode(slideFrame, fps)))
+            # print ( fps, 
+            #         slideRatio, 
+            #         slideFrame, 
+            #         timecode_to_frames(self.currentClipInfo["attributes"]["Starting TC"], fps), 
+            #         int(self.currentClipInfo["framecount"]), 
+            #         frames_to_timecode(slideFrame, fps))
             if self.kipro:
                 transportState = self.kipro.getTransporterState()[1] # Transport is paused in cueToTimecode
                 self.kipro.cueToTimecode(frames_to_timecode(slideFrame, fps))
