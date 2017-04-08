ed0dc2275e79378f27336c498d56e31279e8c01b
stu940652 <stu940652@0a505772-8c2b-487d-83c2-34df072d0b86>
2017-04-08 00:26:18 +0000

    Started adding control of projector input.

--- trunk/panel_projectors.py
+++ trunk/panel_projectors.py
@@ -14,7 +14,7 @@ class PanelProjector (wx.Panel):
 
         panelSizer = wx.BoxSizer(wx.VERTICAL)
         panelSizer.Add(wx.StaticText(self, -1, "Projectors"))
-        sizer = wx.FlexGridSizer(cols = 10, vgap = 5, hgap = 5)
+        sizer = wx.FlexGridSizer(cols = 12, vgap = 5, hgap = 5)
         
         self.groups = collections.defaultdict(lambda: list())
         
@@ -40,13 +40,19 @@ class PanelProjector (wx.Panel):
             group = Settings.Config.get("projector", "group%i" % i, fallback="sides")
             sizer.Add(wx.StaticText(self, -1, name))
             
-            onoffText = wx.TextCtrl (self, -1, "offline", style=wx.TE_READONLY)
+            onoffText = wx.TextCtrl (self, -1, "offline", size=(50,-1), style=wx.TE_READONLY)
             sizer.Add(onoffText)
             
             shutterCheck = wx.CheckBox(self, -1, label = "Shuttered")
             self.Bind(wx.EVT_CHECKBOX, self.OnShutter, shutterCheck)
             sizer.Add(shutterCheck)
             
+            inputCombo = wx.ComboBox(self, -1, style=wx.CB_READONLY)
+            sizer.Add(inputCombo)
+            for input_type, input_index in proj.get_inputs():
+                inputCombo.Append("%s %i" % (input_type, input_index))
+            inputCombo.SetValue("%s %i" % proj.get_input())
+            self.Bind(wx.EVT_COMBOBOX, self.OnInputChange, inputCombo)
             
             if Settings.Config.getboolean("projector", "onoffcontrol", fallback=False):            
                 on_button = wx.Button (self, -1, "On")
@@ -62,12 +68,10 @@ class PanelProjector (wx.Panel):
                 sizer.AddStretchSpacer(0)
                 sizer.AddStretchSpacer(0)
                 
-            self.projectors.append( (proj, onoffText, shutterCheck, on_button, off_button) )
+            self.projectors.append( (proj, onoffText, shutterCheck, on_button, off_button, inputCombo) )
             if group:
-                self.groups[group].append( (proj, onoffText, shutterCheck, on_button, off_button) )
+                self.groups[group].append( (proj, onoffText, shutterCheck, on_button, off_button, inputCombo) )
             
-
-        
         panelSizer.Add(sizer, border = 5, proportion=0, flag=wx.ALIGN_CENTER_HORIZONTAL|wx.ALL)
         
         self.SetSizer(panelSizer)
@@ -87,29 +91,53 @@ class PanelProjector (wx.Panel):
         if group and group in self.groups:
             proj_list = self.groups[group]
             
-        for proj, onoffText, shutterCheck, on_button, off_button in proj_list:
+        for proj, onoffText, shutterCheck, on_button, off_button, inputCombo in proj_list:
             if proj:
                 proj.set_mute(pjlink.MUTE_VIDEO | pjlink.MUTE_AUDIO, shutter)
                 
     def OnShutter (self, evt):
-        for proj, onoffText, shutterCheck, on_button, off_button in self.projectors:
+        for proj, onoffText, shutterCheck, on_button, off_button, inputCombo in self.projectors:
             if proj and (shutterCheck == evt.GetEventObject()):
                 proj.set_mute(pjlink.MUTE_VIDEO | pjlink.MUTE_AUDIO, shutterCheck.GetValue())
         
+    def SetInput (self, s, group):
+        input_name, input_index = s.split(" ")
+        proj_list = self.groups[group]
+            
+        for proj, onoffText, shutterCheck, on_button, off_button, inputCombo in proj_list:
+            if proj:
+                proj.set_input(input_name, input_index)
+            
+    def GetInput (self, s, group):
+        input_name, input_index = s.split(" ")
+        proj_list = self.groups[group]
+        
+        ret_val = []
+        for proj, onoffText, shutterCheck, on_button, off_button, inputCombo in proj_list:
+            if proj:
+                ret_val.append("%s %i" % proj.get_input())
+            
+    def OnInputChange (self, evt):
+        for proj, onoffText, shutterCheck, on_button, off_button, inputCombo in self.projectors:
+            if proj and (inputCombo == evt.GetEventObject()):
+                #print (inputCombo.GetValue())
+                pass
+                # proj.set_input(inputCombo.GetValue().split(" "))
+        
     def OnDisplayOnButton (self, evt):
-        for proj, onoffText, shutterCheck, on_button, off_button in self.projectors:
+        for proj, onoffText, shutterCheck, on_button, off_button, inputCombo in self.projectors:
             if proj and (on_button == evt.GetEventObject()):
                 proj.set_power("on")
         self.OnTimer(None)
         
     def OnDisplayOffButton (self, evt):
-        for proj, onoffText, shutterCheck, on_button, off_button in self.projectors:
+        for proj, onoffText, shutterCheck, on_button, off_button, inputCombo in self.projectors:
             if proj and (off_button == evt.GetEventObject()):
                 proj.set_power("off")
         self.OnTimer(None)
         
     def OnTimer (self, evt):
-        for proj, onoffText, shutterCheck, on_button, off_button in self.projectors:
+        for proj, onoffText, shutterCheck, on_button, off_button, inputCombo in self.projectors:
             if proj:
                 try:
                     onoffText.SetValue(proj.get_power())
